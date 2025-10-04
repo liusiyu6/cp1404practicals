aa17986c895c1d63cb915a0e6d0f92ebc890da30
刘思宇 <837266815@qq.com>
2025-10-04 22:14:08 +0800

    Add random numbers

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,16 @@
 MINIMUM_LENGTH = 6
-password = input("Enter your password: ")
-if len(password) < MINIMUM_LENGTH:
-    print("Your password is too short")
+def main():
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
+
+def get_password(MINIMUM_LENGTH):
     password = input("Enter your password: ")
-print("*" * len(password))
+    if len(password) < MINIMUM_LENGTH:
+        print("Your password is too short")
+        password = input("Enter your password: ")
+    return password
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+main()
--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,20 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score_result(score)
+    print(result)
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def determine_score_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
+
