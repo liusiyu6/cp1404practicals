6d7ba15f42d8557646168724b79e25601bf51c84
刘思宇 <837266815@qq.com>
2025-10-04 22:13:48 +0800

    Add random numbers

---
+++ prac_03/randoms.py
@@ -0,0 +1,23 @@
+# a
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 6, the largest is 20
+
+# b
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# The smallest number is 3, the largest is 9
+
+# c
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 2.5499180693464814, the largest is 5.483596227131856
+
+# d
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+import random
+print(random.randint(1, 100))
+
+
