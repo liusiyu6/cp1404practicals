46b7bcfc14284398fbff3699adc9f07487aa6d06
刘思宇 <837266815@qq.com>
2025-11-02 15:06:38 +0800

    Add ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,20 @@
+"""CP1404/CP5632 Practical - Programming Language class example."""
+
+
+class ProgrammingLanguage:
+    """Represent a Programming Language object."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True if the language is dynamically typed."""
+        return self.typing.lower() == "dynamic"
+
+    def __str__(self):
+        """Return a nicely formatted string representation of the language."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
